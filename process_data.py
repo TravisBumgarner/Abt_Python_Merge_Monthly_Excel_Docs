5243b3ab63b63c64c5a04379d50de50c86fa38df
tbumgarner <travis_bumgarner@abtassoc.com>
2016-11-28 12:10:27 -0500

    Working through updating dates on files now

--- process_data.py
+++ process_data.py
@@ -9,6 +9,9 @@ results = "results.csv"
 template_file = "2016-07.csv" #Specify oldest file to act as the template for results.csv
 columns_to_keep = [0,1,2]
 column_with_unique_id = 0
+qty_columns_to_keep = columns_to_keep[:]
+qty_columns_to_keep.remove(column_with_unique_id)
+qty_columns_to_keep = len(qty_columns_to_keep)
 
 
 ##################################################################
@@ -39,13 +42,13 @@ merged_columns = []
 for column in columns_without_id:
 	merged_columns.append(base[0][column] + "- Mod Date")
 
-#Created Merged Document Headers
+#Create Merged Document Headers
 merged = [base_extract[0] + merged_columns]
 
 ##################################################################
 #Loop through the remaining .csv files ###########################
 ##################################################################
-def merge_and_date_csv_files(csv_directory, merged_result):
+def merge_and_date_csv_files(csv_directory, merged_result,id_column):
 	list_of_csv_files = []
 	for root, dirs, csv_files, in os.walk(csv_directory):
 		for csv_file in csv_files:
@@ -54,12 +57,38 @@ def merge_and_date_csv_files(csv_directory, merged_result):
 
 	for csv_file in list_of_csv_files:
 		csv_as_a_list = open_csv_as_list(csv_directory + "/" + csv_file)
-		csv_as_a_list_extracted = extract_columns(csv_as_a_list,columns_to_keep)
+		list_looping = extract_columns(csv_as_a_list,columns_to_keep)
 		#Append Rows to merged doc
-		for row in range(1,len(csv_as_a_list_extracted)):
-			merged_result.append(csv_as_a_list_extracted[row])
+		#If the merged csv only has the headers, add entries from the first csv file
+		if len(merged_result) == 1:
+			for row_looping in range(1,len(list_looping)):
+				merged_result.append(list_looping[row_looping])
+		else:
+			for row_looping in range(1,len(list_looping)):
+				for row_merged in range(1, len(merged_result)):
+					#id column is the column that has the unique table ID
+					merged_id = merged[row_merged][id_column]
+					looping_id = list_looping[row_looping][id_column]
+					#If the id in each of the two lists are the same, check each cell to see if anything has changed
+					if (merged_id == looping_id):
+						for cell in range(0, len(merged[row_merged])):
+							if merged[row_merged][cell] != list_looping[row_looping][cell]:
+								merged[row_merged][cell] = list_looping[row_looping][cell]
+								merged[row_merged][cell + qty_columns_to_keep] = csv_file[:-5]
 
-merge_and_date_csv_files(csv_directory, merged)
+
+					#Else if the looping_id was not found in the merged csv, append it to the end
+					elif ((merged_id != looping_id) and (row_looping == len(list_looping)+1)):
+						list_looping.append(list_looping[row_looping])
+
+
+
+
+
+
+
+
+merge_and_date_csv_files(csv_directory, merged, column_with_unique_id)
 
 
 def save_list_as_csv(csv_list,file_name):
